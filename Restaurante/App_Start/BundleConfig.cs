aff11fe2b18f7a3b24cc344340c8e15f0fa5f378
Ignacio Gaston Segura <igna.segura94@gmail.com>
2019-06-25 22:33:43 -0300

    25/06 parte 2. Agregué el botón, pero no lo muestra bien. Llevar el atributo nuevo a todos lados y crear métodos para modificar.

--- Restaurante/App_Start/BundleConfig.cs
+++ Restaurante/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Restaurante
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/all.min.css",
                       "~/Content/site.css"));
         }
     }
